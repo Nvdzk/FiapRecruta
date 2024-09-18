10ffca6370d40dae0fd6d7be32d8b16f4503a1d3
Isaac Damasceno <isaacnvdamasceno@gmail.com>
2024-09-17 21:46:53 -0300

    fix code style

--- src/main/java/br/com/recruta/beans/User.java
+++ src/main/java/br/com/recruta/beans/User.java
@@ -10,29 +10,70 @@ import jakarta.persistence.Table;
 @Entity
 @Table(name="TB_USER")
 public class User {
-	
-	// Define se o usuario é um candidato ou recrutador
 	@Column(name = "role", nullable = false, columnDefinition = "VARCHAR(255) DEFAULT 'user")
 	private String role;
 
 	@Column(name="id")
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(name="nome", length = 50)
+	private String nome;
+	
+	@Column(name="email", length = 70)
+	private String email;
+	
+	@Column(name="telefone", length = 13)
+	private String telefone;
 
+	@Column(name="senha", length = 12)
+	private String senha;
+	
+	@Column(name="foto", length = 100)
+	private String foto;
 
+	public int getId() {
+		return id;
+	}
 
+	public void setId(int id) {
+		this.id = id;
+	}
 
+	public String getNome() {
+		return nome;
+	}
 
-// nome
-// email
-// telefone
-// foto
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
 
+	public String getEmail() {
+		return email;
+	}
 
-// sexo
-// cidade
-// estado
-// endereco
-// link para rede social
-// corpo do curriculo 
-// formacao academica 
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getFoto() {
+		return foto;
+	}
+}
